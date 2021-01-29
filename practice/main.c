ff7497b640404eeebcb5311f2f0d9f5943caef1e
Jorge Avila <jorge.avila@mavs.uta.edu>
2021-01-28 21:10:46 -0600

    turned on blue led on using gpio.h

--- practice/main.c
+++ practice/main.c
@@ -1,9 +1,32 @@
+//Jorge Avila
 
+#include "clock.h"
+#include "gpio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "wait.h"
 
-/**
- * main.c
- */
+//define
+#define BLUE_LED PORTF,2 //PF2
+
+//prototypes
+void initHw();
+
 int main(void)
 {
-	return 0;
+    initHw();
+
+}
+void initHw()
+{
+    // Initialize system clock to 40 MHz
+    initSystemClockTo40Mhz();
+
+    // Enable clocks
+    enablePort(PORTF);
+
+    // Configure LED and pushbutton pins
+    selectPinPushPullOutput(BLUE_LED);
+    setPinValue(BLUE_LED, 1);
+    while(true);
 }
